c4d661c34f65a9b69882622d757c3bf866ada452
lvalentine6 <ysr1227@naver.com>
2024-04-24 00:57:51 +0900

    fix: add font awesome

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import '../style.css';
 import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faGithub)
 
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.use(store);
 app.mount('#app');
