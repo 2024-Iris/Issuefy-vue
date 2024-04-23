1f6fe6b9973c5001ae7df4fcf95bcd923c7412db
lvalentine6 <ysr1227@naver.com>
2024-04-23 18:58:59 +0900

    feat: add header, HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+      <p class="text-base font-semibold text-indigo-600">404</p>
+      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">페이지를 찾을수 없습니다.</h1>
+      <p class="mt-6 text-base leading-7 text-gray-600">본문 내용</p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
