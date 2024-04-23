9895eba214ff04d89124c9c357b795c705c223b2
lvalentine6 <ysr1227@naver.com>
2024-04-23 21:37:41 +0900

    fix: add isLogin tag

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,16 @@
+<script setup>
+import store from "@/store";
+const isLogin = store.getters.isLoggedIn
+</script>
+
 <template>
   <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div v-if="isLogin">
+      <p>로그인 됨</p>
+    </div>
+    <div v-else>
+      <p>로그인 안됨</p>
+    </div>
     <div class="text-center">
       <p class="text-base font-semibold text-indigo-600">404</p>
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">페이지를 찾을수 없습니다.</h1>
@@ -7,10 +18,6 @@
     </div>
   </main>
 </template>
-
-<script setup>
-
-</script>
 
 <style scoped>
 
