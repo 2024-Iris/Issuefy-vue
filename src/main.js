cfe62bf42f950c399d9768f0a7e4ac95792fbc61
lvalentine6 <ysr1227@naver.com>
2024-04-27 20:34:21 +0900

    fix: add use pinia

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import store from './store';
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {createPinia} from "pinia";
 
 library.add(faGithub)
 
 const app = createApp(App);
+const pinia = createPinia()
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.use(store);
+app.use(pinia)
 app.mount('#app');
