2dc02f16f88c5558f80909344275bcdc3025a8a1
lvalentine6 <ysr1227@naver.com>
2024-04-28 23:53:22 +0900

    feat: add logout component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,22 +1,15 @@
 <script setup>
-import store from "@/store";
 import {ref, watchEffect} from "vue";
 
 const isLogin = ref(false);
 
 watchEffect(() => {
-  isLogin.value = store.getters.isLoggedIn;
+  isLogin.value = true;
 });
 </script>
 
 <template>
   <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div v-if="isLogin">
-      <p>로그인 됨</p>
-    </div>
-    <div v-else>
-      <p>로그인 안됨</p>
-    </div>
     <div class="text-center">
       <p class="text-base font-semibold text-indigo-600">404</p>
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">페이지를 찾을수 없습니다.</h1>
---
+++ src/components/LogoutPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {useAuthStore} from "@/store/auth";
+import {useRouter} from "vue-router";
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+authStore.clearCredentials()
+router.push('/login')
+</script>
+
+<template>
+<p></p>
+</template>
+
