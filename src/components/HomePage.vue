9312e8be378fe2ac0bed4c7d2ef6ae0843b7f6e6
lvalentine6 <ysr1227@naver.com>
2024-05-15 23:28:21 +0900

    feat: 홈 대시보드 구현

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,43 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1>홈 페이지</h1>
+  <div class="container mx-auto mt-6 max-w-7xl font-sans">
+    <div class="text-black py-4 px-6 flex justify-between items-center font-bold">
+      <h1 class="text-base text-left">대시보드</h1>
+    </div>
+
+    <section class="mt-6">
+      <h2 class="text-lg font-bold">스타 리포지토리</h2>
+      <RepositoryList :starred="true"/>
+    </section>
+
+    <section class="mt-6">
+      <h2 class="text-lg font-bold">스타 이슈</h2>
+      <IssueList :org="org" :repository="repository" :starred="true"/>
+    </section>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import RepositoryList from '@/components/RepositoryPage.vue'
+import IssueList from '@/components/IssuePage.vue'
 
+export default {
+  name: 'HomePage',
+  components: {
+    RepositoryList,
+    IssueList
+  },
+  data() {
+    return {
+      org: 'elastic',
+      repository: 'kibana'
+    };
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-top: 1.5rem;
+  margin-bottom: 0.5rem;
+}
 </style>
