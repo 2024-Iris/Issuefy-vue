7f9bfb1966bb2dfb7c4daec2266e6d4fea67f252
lvalentine6 <ysr1227@naver.com>
2024-05-18 04:33:50 +0900

    feat: 리포지토리 추가시 인풋 화면 밖을 누르면 취소 로직 구현

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import {createPinia} from "pinia";
+import clickOutside from './directives/clickOutside';
 
 library.add(faGithub)
 
@@ -14,4 +15,5 @@ const pinia = createPinia()
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.use(pinia)
+app.directive('click-outside', clickOutside);
 app.mount('#app');
