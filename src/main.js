5bc0cab8dbb75ab138b930e14ffbda2ddc1436cf
lvalentine6 <ysr1227@naver.com>
2024-07-03 21:34:54 +0900

    fix: 중복 임포트 제거

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../style.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
-import { faCircleExclamation, faCheck } from '@fortawesome/free-solid-svg-icons'
+import {faBell, faCheck, faCircleExclamation} from '@fortawesome/free-solid-svg-icons'
 import {createPinia} from "pinia";
 import clickOutside from './directives/clickOutside';
 import {useAuthStore} from "@/store/pinia";
-import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faGithub, faCircleExclamation, faCheck, faBell)
 
