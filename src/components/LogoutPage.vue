a7f94b3e818e482ba266a519c378103cadee3312
lvalentine6 <ysr1227@naver.com>
2024-07-22 01:56:31 +0900

    feat: 로그아웃시 토큰 무효화 요청 추가

--- src/components/LogoutPage.vue
+++ src/components/LogoutPage.vue
@@ -1,13 +1,31 @@
 <script setup>
-
 import {useAuthStore} from "@/store/pinia";
 import {useRouter} from "vue-router";
+import axios from "axios";
+import {onMounted} from 'vue';
 
 const authStore = useAuthStore()
 const router = useRouter()
 
-authStore.clearCredentials()
-router.push('/login')
+async function performLogout() {
+  try {
+    const accessToken = authStore.accessToken;
+    await axios.post(`${process.env.VUE_APP_API_URL}/logout`, {}, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    await authStore.clearCredentials()
+    await router.push('/login')
+  } catch (error) {
+    console.error('Logout failed:', error);
+  }
+}
+
+onMounted(() => {
+  performLogout();
+})
 </script>
 
 <template>
